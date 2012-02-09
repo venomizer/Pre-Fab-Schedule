4e54f59833e7f5fee5799d0771db5dc0fc805695
Julian Galindo <galindo.julian@gmail.com>
2012-02-09 16:58:10 -0600

    Updated sort code for jobs table

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,9 @@ class Job < ActiveRecord::Base
   belongs_to :customer
   has_many :job_items
   validates :priority, :uniqueness => true
+
+  def priority_update(n)
+    Job.where("priority >=?",n).update_all("priority = priority + 1")
+  end
+
 end
