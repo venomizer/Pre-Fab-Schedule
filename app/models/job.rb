d368919800eeab451e414a276c195e5d9f417dd4
Sean Hill <seandhi@gmail.com>
2012-02-09 17:06:40 -0600

    Add before_validation method to increment the priority to prevent a uniquness collision.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,15 @@
 class Job < ActiveRecord::Base
   belongs_to :customer
   has_many :job_items
+  
+  before_validation :update_priorities
+  
   validates :priority, :uniqueness => true
+  
+  private 
+  
+  def update_priorities
+    self.class.where("priority >= ?", self.priority).update_all("priority = priority + 1")
+  end
+  
 end
