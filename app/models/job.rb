414ffc66689e484dff20f27a642833ef2fd4d59d
Julian Galindo <galindo.julian@gmail.com>
2012-02-13 15:26:26 -0600

    Added validation to priority update to prevent priority updates without an actual update.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,15 +1,21 @@
 class Job < ActiveRecord::Base
+  include ActiveModel::Dirty
   belongs_to :customer
   has_many :job_items
   
   before_validation :update_priorities
   
   validates :priority, :uniqueness => true
-  
+
   private 
   
   def update_priorities
-    self.class.where("priority >= ?", self.priority).update_all("priority = priority + 1")
+    if self.priority_changed?
+      self.class.where("priority >= ?", self.priority).update_all("priority = priority + 1")
+    else
+      nil
+    end
+
   end
   
 end
