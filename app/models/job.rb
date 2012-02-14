f8ad9803375198a3133b65b5951cb6ed7547fb63
Julian Galindo <galindo.julian@gmail.com>
2012-02-14 17:07:14 -0600

    added search function

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,13 +2,21 @@ class Job < ActiveRecord::Base
   include ActiveModel::Dirty
   belongs_to :customer
   has_many :job_items
-  
+
   before_validation :update_priorities
-  
+
   validates :priority, :uniqueness => true
 
-  private 
-  
+  def self.search(search)
+    if search
+      where('number LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
+
+  private
+
   def update_priorities
     if self.priority_changed?
       self.class.where("priority >= ?", self.priority).update_all("priority = priority + 1")
