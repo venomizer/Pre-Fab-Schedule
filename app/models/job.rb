ad712360ce0881dd93e1afc6a61c22748cf3f8ea
Julian Galindo <galindo.julian@gmail.com>
2012-02-22 16:09:29 -0600

    Attempted to create loop to udpate priorities properly.

--- app/models/job.rb
+++ app/models/job.rb
@@ -19,11 +19,21 @@ class Job < ActiveRecord::Base
 
   def update_priorities
     if self.priority_changed?
-      self.class.where("priority >= ?", self.priority).update_all("priority = priority + 1")
+      if self.priority < self.priority_was
+        while self.class.find(:last, priority + 1).priority - self.priority == 1 and self.priority == self.priority.find(:all, priority)
+          self.class.where("priority >= ?", self.priority).update_all("priority = priority + 1")
+        end
+      end
+
+      if self.priority > self.priority_was
+        while self.priority - self.class.find(:last, self.priority - 1).priority == 1
+          self.class.where("priority >= ?", self.priority).update_all("priority = priority + 1")
+          self.class.where("priority < ?", self.priority).update_all("priority = priority - 1")
+        end
+      end
     else
       nil
     end
-
   end
   
 end
