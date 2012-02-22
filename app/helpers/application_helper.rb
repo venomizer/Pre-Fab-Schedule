d83660d3389ce533d4206e6326497fca2c337f92
Julian Galindo <galindo.julian@gmail.com>
2012-02-22 16:08:53 -0600

    added button to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,20 @@ module ApplicationHelper
     link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
   end
 
+      #def button(name,url)
+  def button(*args)
+    if args.size == 2
+      name = args[0]
+      url = args[1]
+      content_tag :button, :type => "button", :onclick => "window.location.href =  '#{url_for(url)}'; " do
+        "#{name}"
+      end
+    elsif args.size == 1
+      name = args[0]
+      content_tag :button, :type => "button" do
+        "#{name}"
+      end
+    end
+  end
+
 end
