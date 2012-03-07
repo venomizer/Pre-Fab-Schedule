2d3120b33e6915619acb887cc13a1ab9e8178d25
Julian Galindo <galindo.julian@gmail.com>
2012-03-07 08:49:10 -0600

    removed username input from the users table.

--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,6 @@ ActiveRecord::Schema.define(:version => 20120301225158) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username",                     :null => false
     t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
